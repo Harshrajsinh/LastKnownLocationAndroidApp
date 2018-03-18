f86e5c9dd691abe52d7e1190d5b65458490657e8
Harshrajsinh Thakor <hrthakor7@gmail.com>
2018-03-17 18:48:41 -0700

    Added the Constants file

---
+++ app/src/main/java/com/example/harshraj/sampleapplication/Constants.kt
@@ -0,0 +1,29 @@
+package com.example.harshraj.sampleapplication
+
+/**
+ * Created by Harshraj on 18-02-2018.
+ * Constants Class where all strings are defined. 
+ */
+
+object Constants {
+
+    val SUCCESS = 0
+    val FAILURE = 1
+    val PACKAGE_NAME = "com.example.harshraj.sampleapplication"
+    val RECEIVER = PACKAGE_NAME + ".RECEIVER"
+    val LOCATION_DATA = PACKAGE_NAME + ".LOCATION_DATA"
+
+    val COUNTRY_NAME = "countryName"
+    val STATE_NAME = "stateName"
+    val POSTAL_CODE = "postalCode"
+    val CITY_NAME = "cityName"
+    val STREET_NAME = "streetName"
+
+    val LOCATION_NOT_FOUND = "Locaation Not Found";
+    val GEOCODER_IS_NOT_PRESENT = "GeoCoder is not present"
+
+    val SERVICE_NOT_AVAILABLE = "Service not Available"
+    val WRONG_COORDINATES = "Invalid Lat and Long"
+    val NO_ADDRESS_NOT_FOUND = "Address not found"
+
+}
